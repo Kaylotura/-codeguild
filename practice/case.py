924d33e94bef3c06c3452c132f0c38ae621de67a
Katie Nichols <Kaylotura@gmail.com>
2016-07-14 10:51:43 -0700

    After learning a bit about list comprehensions, changed code to contain fewer comprehensions still not functioning, but closer now

--- practice/case.py
+++ practice/case.py
@@ -1,5 +1,5 @@
 import string
-
+import re
 """ Takes input in CamelCase, CONSTANT_CASE, kebab-case, or snake_case and converts to any of the others."""
 
 LOWERCASE_STRING = string.ascii_lowercase
@@ -8,7 +8,7 @@ UPPERCASE_LIST = list(string.ascii_uppercase)
 
 def input_phrase_to_translate():
     """ Gets phrase to be translated from user input"""
-    entered_phrase = (input('Please enter a phrase in CamelCase, CONSTANT_CASE, kebab-case, or snake_case. ')).split()
+    entered_phrase = input('Please enter a phrase in CamelCase, CONSTANT_CASE, kebab-case, or snake_case. ')
     if entered_phrase == '':
         entered_phrase = 'snake_case_test'
     return entered_phrase
@@ -34,21 +34,21 @@ def input_new_case():
     return entered_case
 
 
-def check_old_case(cased_old_phrase):
-    """Checks the original case of the phrase to comment on it
-
-    >>> check_old_case(['i_eat_kebabs']):
-    'snake_case'
-    """
-    if '-' in str(cased_old_phrase):
-        old_case = 'kebab-case'
-    elif '_' in str(cased_old_phrase) and any(LOWERCASE_STRING) in str(cased_old_phrase):
-        old_case = 'snake_case'
-    elif '_' in str(cased_old_phrase) and any(LOWERCASE_STRING) not in str(cased_old_phrase):
-        old_case = 'CONSTANT_CASE'
-    elif any (LOWERCASE_STRING).split() in str(cased_old_phrase):
-        old_case = 'CamelCase'
-    return old_case
+# def check_old_case(cased_old_phrase):
+#     """Checks the original case of the phrase to comment on it
+#
+#     >>> check_old_case(['i_eat_kebabs']):
+#     'snake_case'
+#     """
+#     if '-' in str(cased_old_phrase):
+#         old_case = 'kebab-case'
+#     elif '_' in str(cased_old_phrase) and any(LOWERCASE_STRING) in str(cased_old_phrase):
+#         old_case = 'snake_case'
+#     elif '_' in str(cased_old_phrase) and any(LOWERCASE_STRING) not in str(cased_old_phrase):
+#         old_case = 'CONSTANT_CASE'
+#     if [for letter in (LOWERCASE_STRING)] in str(cased_old_phrase):
+#         old_case = 'CamelCase'
+#     return old_case
 
 
 def slice_words_apart(unsliced_phrase):
@@ -62,10 +62,13 @@ def slice_words_apart(unsliced_phrase):
     >>> slice_words_apart(DO_NOT_HUG_ME_I_AM_SCARED)
     ' DO NOT HUG ME I AM SCARED'
     """
-    dash_sliced_phrase = [word.replace('-',' ') for word in unsliced_phrase]
-    underscore_sliced_phrase = [word.replace('',' ') for word in unsliced_phrase]
-    x = [letter.index() for letter in underscore_sliced_phrase if letter in UPPERCASE_LIST]
-    spaced_phrase = underscore_sliced_phrase.insert(' ', x - 1)
+    if '-' in unsliced_phrase:
+        sliced_list = unsliced_phrase.split('-')
+    elif '_' in unsliced_phrase:
+        sliced_list = unsliced_phrase.split('_')
+    else:
+        sliced_list = re.findall('[A-Z][^A-Z]*', unsliced_phrase)
+    spaced_phrase = ' '.join(sliced_list)
     return spaced_phrase
 
 
@@ -74,20 +77,22 @@ def standardize_phrase_lower(phrase):
     >>> standardize_phrase_lower('You are the weakest link. Goodbye.')
     'You are the weakest link. Goodbye.'
     """
-    return phrase.lowercase()
+    return phrase.lower
 
 
 def alter_capitalization_to_new_case(phrase, case):
     """Checks for the desired case, then capitalizes appropriately.
     >>> alter_capitalization_to_new_case(['apple', 'core'], 'CONSTANT_CASE')
-    APPLE CORE
+    'APPLE CORE'
     >>> alter_capitalization_to_new_case(['want', 'some', 'more'], 'CamelCase')
-    Apple Core
-       """
+    'Apple Core'
+    """
     if case == 'CamelCase':
-        capitalized_phrase = [word.capitalize for word in phrase]
+        capitalized_phrase = phrase.capitalize()
     elif case == 'CONSTANT_CASE':
-        capitalized_phrase = phrase.uppercase
+        capitalized_phrase = phrase.upper()
+    else:
+        capitalized_phrase = phrase
     return capitalized_phrase
 
 
@@ -99,24 +104,23 @@ def paste_phrase(phrase):
     return str(phrase).replace(' ','')
 
 
-def output(old_phrase, old_case, new_phrase, new_case):
+def output(old_phrase, new_phrase, new_case):
     """Outputs the translation.
     output(pancakes_taste_good, snake_cake, PancakesTasteGood, CamelCase)
     PancakesTasteGood
     """
-    print ("{} translated from {} to {} is {}!").format(old_phrase, old_case, new_case, new_phrase)
-
+    print(old_phrase + " translated to " + new_case + " is " + new_phrase + "!")
 
 def main ():
     """Main Function"""
     phrase_to_translate = input_phrase_to_translate()
     new_case = input_new_case()
-    old_case = check_old_case(phrase_to_translate)
+#    old_case = check_old_case(phrase_to_translate)
     phrase_with_spaces = slice_words_apart(phrase_to_translate)
     standardized_phrase_lower = standardize_phrase_lower(phrase_with_spaces)
     appropriately_capitalized_phrase = alter_capitalization_to_new_case(standardized_phrase_lower, new_case)
     final_phrase = paste_phrase(appropriately_capitalized_phrase)
-    output(phrase_to_translate, old_case, new_case, final_phrase)
+    output(phrase_to_translate, final_phrase, new_case)
 
 
 if __name__ == '__main__':
