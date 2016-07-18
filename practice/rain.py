0c93fbb647f4670c9eeb7202113ea551a09cf604
Katie Nichols <Kaylotura@gmail.com>
2016-07-18 10:40:03 -0700

    Have code for solving for day with most rain largely written Working on output, and year

--- practice/rain.py
+++ practice/rain.py
@@ -1,4 +1,6 @@
-"""" Opens a Rain Text File and Mines for information"""
+"""" Opens a Rain Text File and Mines for information
+I'm using "cells" for rows, and "boxes" for columns
+"""
 
 def open_file():
     """Opens text file for analysis"""
@@ -7,35 +9,79 @@ def open_file():
     return lines_of_text
 
 
+
+def cut_non_data(text):
+    """ Takes in lines of text, and only returns relevant data lines.
+    >>> cut_non_data(['I can haz cheezeburger', /
+    >>> '------------------------------------------------------------------------------------------------------------------\n', /
+    >>>> 'This line is the only one that matters'])
+    ['This is the only one that matters']
+    """
+    TABLE_LINE_CUT_OFF = '------------------------------------------------------------------------------------------------------------------\n'
+    table_start_index = text.index(TABLE_LINE_CUT_OFF) + 1
+    table_as_text_lines = text[table_start_index:]
+    return table_as_text_lines
+
+
 def split_single_line_to_single_row(text):
     """Splits a single line of text into a row in the form of a list of strings"""
     return text.split(' ')
+
 
 def split_lines_to_rows(text):
     """Splits all lines of text into several rows in the form of lists of strings
     >>> split_lines_to_rows([['Charmander Bulbasuar Squirtle'],['Ponya Oddish Magicarp'],['Fire Plant Water']])
     [['Charmander', 'Bulbasuar', 'Squirtle'],['Ponya', 'Oddish', 'Magicarp'],['Fire', 'Plant', 'Water']]
-      """
-    rows = [split_lines_to_rows(line) for line in text]
+    """
+    rows = [split_single_line_to_single_row(line) for line in text]
     return rows
 
-def convert_row_to_column(row):
-    """Takes a single row as lists of strings, and transforms it into a column as lists of strings"""
-    column = list(zip(*row))
 
 def convert_rows_to_columns(rows):
     """Takes rows as lists of strings, and transforms them into columns as lists of strings
     >>> [['Charmander', 'Bulbasuar', 'Squirtle'],['Ponya', 'Oddish', 'Magicarp'],['Fire', 'Plant', 'Water']]
     [['Charmander', 'Ponya'', 'Fire'],['Bulbasuar', 'Oddish', 'Plant'],['Squirtle', 'Magicarp', 'Water']]
     """
-    columns = [convert_row_to_column(row) for row in rows]
+    columns = list(zip(*rows))
+    return columns
 
+
+def get_most_rain_amount(columns):
+    """Gets the value of the most rain for any given date
+    >>> get_most_rain_amount([['1', '2', '3', '8'], ['4', '5', '6', '1'], ['7', '8', '9', '12']])
+    '6'
+    """
+    most_rain = max(columns[1])
+    return most_rain
+
+
+def get_index_of_max_daily_rain_box(columns):
+    """Takes in rows of data, and returns index of day with most rain columns max rain
+    >>> get_day_with_most_rain([['1', '2', '3', '8'], ['4', '5', '6', '1'], ['7', '8', '9', '12']])
+    '2'
+    """
+    index_of_max_daily_rain = columns[1].index(max(columns[1]))
+    return index_of_max_daily_rain
+
+
+def get_day_with_most_rain (rows, columns):
+    index_of_day_cell = get_index_of_max_daily_rain_box(columns)
+    day_full_line = rows[index_of_day_cell]
+    date = day_full_line[0]
+    return date
+
+def output(most_rain, )
 
 def main():
-    lines_of_text = open_file()
-    rows_of_data = split_lines_to_rows(lines_of_text)
+    text_lines = open_file()
+    table_as_text_lines = cut_non_data(text_lines)
+    rows_of_data = split_lines_to_rows(table_as_text_lines)
+    most_rain_amount_single_date = get_most_rain_amount(columns_of_data)
     columns_of_data = convert_rows_to_columns(rows_of_data)
+    date_with_most_rain = get_day_with_most_rain (rows_of_data, columns_of_data)
+
+    output()
+
 
 if __name__ == '__main__':
     main()
-
