3b90afab3771504149bcab49f402d0910813cd05
Katie Nichols <Kaylotura@gmail.com>
2016-08-04 21:12:02 -0700

    saving progress

---
+++ practice/caesar/site.js
@@ -0,0 +1,24 @@
+'use strict';
+/**
+ * One function converts a string into a ceaser cipher based on a key,
+ * the other returns a translated string based on a decipher key.
+ *
+ * Cipher example: 'cats', 1 -> 'dbut'
+ * Decipher example:  fqiu', 2 -> 'dogs'
+ */
+
+var ALPHA_TO_NUMBER = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+function caesarEncrypt(plainStr, key) {
+  var stringAsArray = plainStr.split('');
+  var i = 0
+
+
+
+
+
+}
+
+
+
+// caesarDecrypt(plainStr, key)
