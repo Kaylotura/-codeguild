a9079547357ca05558bb774b8f5633cc01a0bc91
Katie Nichols <Kaylotura@gmail.com>
2016-08-11 11:02:56 -0700

    basic html built, basic flexbox built, have regular expressions for all three fields.

---
+++ practice/validator/site.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ *
+ */
+var NAME_STUCTURE = /(\w+)\s(\w+)/;
+var BIRTHDAY_STRUCTURE = /(\d\d)\-(\d\d)\-(\d\d\d\d)/;
+var PHONE_NUMBER_STRUCTURE = /(\d\d\d)\-(\d\d\d)\-(\d\d\d\d)/;
+
+/**
+ *
+ */
+// function validatePhoneNumber(string) {
+//    new RegExp('^' + prefix)
+// }
