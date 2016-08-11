8f62f3fc10fee61f6e23bf30c338ffa8774ab6d4
Katie Nichols <Kaylotura@gmail.com>
2016-08-11 16:21:29 -0700

    removed patterns and reduced inefficiency. Still needs polish.

--- practice/validator/site.js
+++ practice/validator/site.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * The Validator takes in the users input as they type into the form in
- * index.html and uses events to visually que the user that their input is not
- * valid, by filling the field with a yellow background. Once the the entry is
+ * The Validator takes in the user's input as they type into the form and
+ * uses kestroke events to visually que the user that their input is not
+ * valid by filling the field with a yellow background. Once the the entry is
  * valid the background returns to white.
  *
  * Currently there are many patterns that need to be taken care of.
@@ -25,75 +25,47 @@ function removeYellowFromField(field) {
 }
 
 /**
+ * Takes in an entry, and the field it came from and returns whether it is a
+ * valid entry or not.
+ */
+function checkIfValid(field, entry) {
+  if (field === 'fullname') {
+    return /(\w+)\s(\w+)/.test(entry);
+  } else if (field === 'birthday') {
+    return /(\d\d\d\d)\-(\d\d)\-(\d\d)/.test(entry);
+  } else if (field === 'phone-number') {
+    return /(\d\d\d)\-(\d\d\d)\-(\d\d\d\d)/.test(entry);
+  }
+}
+
+
+/**
  * Begins by checking the entry in fullname class text box on the form,
  * then proceeds to run that value through a validation process.
  */
+function validateEntry(inputclass) {
+  var entry = $('input.' + inputclass).prop('value');
+  if (inputclass(entry)) {
+    removeYellowFromField(inputclass);
+  } else if (!entry) {
+    removeYellowFromField(inputclass);
+  } else {
+    makeFieldYellow(inputclass);
+  }
+}
+
+
 function runName() {
-  var nameEntry = $('input.fullname').prop('value');
- /**
-  * If the entry in the full name class text box is valid or empty, the
-  * invalid class is turned off for that textbox, but otherwise it is turned
-  * on.
-  */
-  function validateName(nameEntry) {
-    return /(\w+)\s(\w+)/.test(nameEntry);
-  }
-  if (validateName(nameEntry)) {
-    removeYellowFromField('fullname');
-  } else if (!nameEntry) {
-    removeYellowFromField('fullname');
-  } else {
-    makeFieldYellow('fullname');
-  }
+  validateEntry('fullname');
 }
 
-/**
-* Begins by checking the entry in phone-number class text box on the form,
-* then proceeds to run that value through a validation process.
- */
-function runPhoneNumber() {
-  var phoneNumberEntry = $('input.phone-number').prop('value');
-  /**
-   * If the entry in the phone number class text box is valid or empty, the
-   * invalid class is turned off for that textbox, but otherwise it is turned
-   * on.
-   */
-  function validatePhoneNumber() {
-    return /(\d\d\d)\-(\d\d\d)\-(\d\d\d\d)/.test(phoneNumberEntry);
-  }
-  if (validatePhoneNumber(phoneNumberEntry)) {
-    removeYellowFromField('phone-number');
-  } else if (!phoneNumberEntry) {
-    removeYellowFromField('phone-number');
-  } else {
-    makeFieldYellow('phone-number');
-  }
-}
-
-/**
-* Begins by checking the entry in birthday class text box on the form,
-* then proceeds to run that value through a validation process.
- */
 function runBirthday() {
-  var birthdayEntry = $('input.birthday').prop('value');
-  console.dir(birthdayEntry);
- /**
-  * If the entry in the phone number class text box is valid or empty, the
-  * invalid class is turned off for that textbox, but otherwise it is turned
-  * on.
-  */
-  function validateBirthday() {
-    return /(\d\d\d\d)\-(\d\d)\-(\d\d)/.test(birthdayEntry);
-  }
-  if (validateBirthday(birthdayEntry)) {
-    removeYellowFromField('birthday');
-  } else if (!birthdayEntry) {
-    removeYellowFromField('birthday');
-  } else {
-    makeFieldYellow('birthday');
-  }
+  validateEntry('birthday');
 }
 
+function runPhoneNumber() {
+  validateEntry('phone-number');
+}
 
 /**
  * The even handler run the validation that the user is typing indirectly after
