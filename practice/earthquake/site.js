d295b1ac62ba36d5b75df534c5000a344edebd5d
Katie Nichols <Kaylotura@gmail.com>
2016-08-16 21:15:06 -0700

    working on untangling messy code

--- practice/earthquake/site.js
+++ practice/earthquake/site.js
@@ -1,26 +1,8 @@
 'use strict';
 
-if (!window) {
-  var ol = '';
-}
-
-
-var map = new ol.Map({
-  target: 'map',
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM()
-    })
-  ],
-  view: new ol.View({
-    center: ol.proj.fromLonLat([37.41, 8.82]),
-    zoom: 2
-  })
-});
-
-
 /**
- *
+ * Requests the earthquake information for the last seven days from the
+ * earthquake USGS database. It returns this as a promise.
  */
 function getEarthquakes() {
   var url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
@@ -33,19 +15,89 @@ function getEarthquakes() {
 /**
  *
  */
-function tryItOut() {
-  getEarthquakes().
-    then(function(object) {
-      console.dir(object);
-    }).
-   catch(function(error) {
-     console.dir('Error occured. ' + error.statusText);
-   });
+function formatSingleEarthquake(earthquake) {
+  var earthquakeProperies = earthquake.properties;
+  var earthquakeGeometry = earthquake.geometry;
+  var earthquakeTime = earthquakeProperies.time;
+  var earthquakeAgeHours =
+    (new Date().getTime() - earthquakeTime) / (1000 * 60 * 60);
+  var earthquakeAgeHoursClean = earthquakeAgeHours.toFixed(2);
+  var earthquake = {};
+  earthquake.magnitude = earthquakeProperies.mag;
+  earthquake.ageInHours = earthquakeAgeHoursClean;
+  earthquake.XYCoords = earthquakeGeometry.coordinates.slice(0, 2);
+  return earthquake;
 }
 
-$(document).ready(tryItOut);
+/**
+ * Initiated by the event handler, this function fetches the earthquake data
+ * and asynchronistically manipulates it by extracting information from nested
+ * objects & arrays. It then manipulates the date information into an hourly
+ * age. It then returns an array of Earthquake Objects, each with a magnitude,
+ * location by XY coordinates, and an age.
+ */
+function formatEarthquakes(earthquakes) {
+  var earthquakeFeatures = earthquakes.features;
+  var earthquakesAsMagnitutdeAgeCoords = _.map(
+    earthquakeFeatures, formatSingleEarthquake
+  );
+  return earthquakesAsMagnitutdeAgeCoords;
+}
+
+/**
+ *
+ */
+function RenderEarthquakes(earthquakes) {
+  var simpleEarthquakes = formatEarthquakes(earthquakes);
+  var earth = new ol.layer.Tile({
+    source: new ol.source.OSM()
+  });
+  var map = new ol.Map({
+    layers: [earth],
+    target: 'map',
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 2})
+  });
+
+}
+
+function main() {
+var earthquakes = getEarthquakes().
+then(RenderEarthquakes(earthquakes).
+
+}
 
 
+  //       layers = []
+  //       for (var i = 0; i < x.length; i += 1) {
+  //         var layer = new ol.layer.Vector({
+  //           source: new ol.source.Vector({
+  //             features: new ol.Feature({
+  //               'geometry': new ol.Point(
+  //                 earthquakesAsMagnitutdeAgeCoords[i].XYCoords
+  //               )
+  //             })
+  //           }),
+  //           style: new ol.style.Style({
+  //             image: new ol.style.Circle({
+  //               radius: earthquakesAsMagnitutdeAgeCoords[i].magnitude,
+  //               fill: new ol.style.Fill({color: 'FF5050'})
+  //             })
+  //           })
+  //         })
+  //       });
+  //     layers += layer
+
+
+
+// }
+
+$(document).ready(main);
+
+/**
+ * Makes the linter ignore map variable
+ */
 if (!window) {
   print(map);
 }
