23ae846bad2c69bd59e55ebb9312005e9baf6697
Katie Nichols <Kaylotura@gmail.com>
2016-09-02 11:41:47 -0700

    added doctest to logic

--- practice/flutter/flutter/logic.py
+++ practice/flutter/flutter/logic.py
@@ -4,7 +4,12 @@ from . import models
 
 
 def create_and_save_new_flutt(text):
-    """creates and saves a new flutt."""
+    """Creates and saves a new flutt.
+
+    >>> stark = create_and_save_new_flutt('winter is coming')
+    >>> stark.text
+    'winter is coming'
+        """
     new_flutt = models.Flutt(text=text)
     new_flutt.save()
     return new_flutt
