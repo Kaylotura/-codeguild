dd404b419223895c4e917ec9fbfa61692d1d5aaa
Katie Nichols <Kaylotura@gmail.com>
2016-09-08 11:27:51 -0700

    migrated models and added doctests to Flutt Class

--- practice/flutter/flutter/models.py
+++ practice/flutter/flutter/models.py
@@ -4,11 +4,23 @@ from django.db import models
 
 
 class Flutt(models.Model):
-    """A flutt class model that stores text and a timestamp."""
+    """A flutt class model that stores text and a timestamp.
+    """
     text = models.TextField()
     timestamp = models.DateTimeField(auto_now=True)
 
     def __str__(self):
+        """Returns a string that represnts a shorthand description of the flutt.
+        >>> carol = Flutt(text='the moumwraiths outgrabe')
+        >>> carol.__str__()
+        'the moumwraiths outgrabe'
+        """
         return self.text
+
     def __repr__(self):
+        """Returns a string that represnts a shorthand description of the flutt.
+        >>> billy = Flutt(text='dancing with myself, oh-oh', timestamp=3)
+        >>> billy.__repr__()
+        'Flutt(text=dancing with myself, timestamp=3)
+        """
         return 'Flutt(text={!r}, timestamp={!r})'.format(self.text, self.timestamp)
